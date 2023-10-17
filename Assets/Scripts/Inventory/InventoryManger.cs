59eeb83b756e4ceb1e4efc6c5b1cd60bd17411bb
Jacob Asher - Home Desktop <31144233+Jasher1608@users.noreply.github.com>
2023-10-18 10:32:23 +1100

    Set up Inventory UI

--- Assets/Scripts/Inventory/InventoryManger.cs
+++ Assets/Scripts/Inventory/InventoryManger.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InventoryManger : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class InventoryManger : MonoBehaviour
     [SerializeField] private ItemClass itemToAdd;
     [SerializeField] private ItemClass itemToRemove;
 
-    public List<ItemClass> items = new List<ItemClass>();
+    public List<SlotClass> items = new List<SlotClass>();
 
     private GameObject[] slots;
 
@@ -33,25 +34,91 @@ public class InventoryManger : MonoBehaviour
             try
             {
                 slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
-                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].itemIcon;
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().itemIcon;
+                if (items[i].GetItem().isStackable)
+                {
+                    slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = items[i].GetQuantity().ToString();
+                }
+                else
+                {
+                    slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "";
+                }
             }
             catch
             {
                 slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
                 slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+                slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "";
             }
         }
     }
 
-    public void Add(ItemClass item)
+    public bool Add(ItemClass item)
     {
-        items.Add(item);
+        // Check if inventory contains item
+
+        SlotClass slot = Contains(item);
+        if (slot != null && slot.GetItem().isStackable)
+        {
+            slot.AddQuantity(1);
+        }
+        else
+        {
+            if (items.Count < slots.Length) // Check if there is room in the inventory
+            {
+                items.Add(new SlotClass(item, 1));
+            }
+            else
+            {
+                return false;
+            }
+        }
         RefreshUI();
+        return true;
     }
 
-    public void Remove(ItemClass item)
+    public bool Remove(ItemClass item)
     {
-        items.Remove(item);
+        SlotClass temp = Contains(item);
+        if (temp != null)
+        {
+            if (temp.GetQuantity() > 1)
+            {
+                temp.SubQuantity(1);
+            }
+            else
+            {
+                SlotClass slotToRemove = new SlotClass();
+                foreach (SlotClass slot in items)
+                {
+                    if (slot.GetItem() == item)
+                    {
+                        slotToRemove = slot;
+                        break;
+                    }
+                }
+                items.Remove(slotToRemove);
+            }
+            
+        }
+        else
+        {
+            return false;
+        }
+        
         RefreshUI();
+        return true;
+    }
+
+    public SlotClass Contains(ItemClass item)
+    {
+        foreach(SlotClass slot in items)
+        {
+            if (slot.GetItem() == item)
+            {
+                return slot;
+            }
+        }
+        return null;
     }
 }
